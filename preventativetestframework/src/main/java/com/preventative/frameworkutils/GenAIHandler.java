9e1e5e8f6b72d2371d52a5dae2c6042a115183f8
Soham Patel <sohampatel07@outlook.com>
2024-03-27 12:21:38 +0530

    fixing the gpt issue

--- preventativetestframework/src/main/java/com/preventative/frameworkutils/GenAIHandler.java
+++ preventativetestframework/src/main/java/com/preventative/frameworkutils/GenAIHandler.java
@@ -1,5 +1,8 @@
 package com.preventative.frameworkutils;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -19,6 +22,7 @@ public class GenAIHandler {
         String id = "";
         URL url = new URL(this.CHATGPTURL + "/chat/completions");
         HttpURLConnection httpConn = (HttpURLConnection)url.openConnection();
+        InputStream inputStream;
         try {
             httpConn.setRequestMethod("POST");
             httpConn.setRequestProperty("Authorization", "Bearer " + this.API_KEY);
@@ -43,15 +47,25 @@ public class GenAIHandler {
             String soham=httpConn.getOutputStream().toString();
             httpConn.getOutputStream().close();
             int responseCode = httpConn.getResponseCode();
-            if (responseCode != 204 || responseCode!=200) {
-                System.out.print("Response is: "+httpConn.getResponseMessage());
-                throw new HttpResponseException(responseCode, "We are getting other than 200 or 204 for api call");
+            if (200 <= responseCode && responseCode <= 299) {
+                inputStream = httpConn.getInputStream();
+            } else {
+                inputStream = httpConn.getErrorStream();
             }
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(
+                            inputStream));
 
-            System.out.println("We are getting correct response code " + responseCode);
-            System.out.println("Request which were hitting: "+httpConn.getRequestProperties());
-            System.out.println("Response Message is: "+httpConn.getResponseMessage());
-            return httpConn.getResponseMessage();
+            StringBuilder response = new StringBuilder();
+            String currentLine;
+
+            while ((currentLine = in.readLine()) != null)
+                response.append(currentLine);
+            System.out.println("We are getting response code " + responseCode);
+            //System.out.println("Request which were hitting: "+httpConn.getRequestProperties());
+            System.out.println("Response Message is: "+response.toString());
+            in.close();
+            return response.toString();
         } catch (HttpResponseException httpResponseException) {
             httpResponseException.printStackTrace();
             System.out.println("Recommendation not generated");
